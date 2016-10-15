1a2a128d70dcd37e562ad4ca6c9972339f0426d4
Owlz <whootandahalf@gmail.com>
2016-10-15 09:22:48 -0700

    copywrite

--- modules/banner.py
+++ modules/banner.py
@@ -14,13 +14,14 @@ class Banner:
 
         # If we're in too small of an area to actually draw, just type
         if height < 4 or width < 49:
-            return "sharePlayer"
+            return "sharePlayer -- {0}".format(url)
 
         else:
             return banner
 
+url = '(c) https://github.com/Owlz/sharePlayer'
 
 banner = """  ___ / /  ___ ________ / _ \/ /__ ___ _____ ____
  (_-</ _ \/ _ `/ __/ -_) ___/ / _ `/ // / -_) __/
 /___/_//_/\_,_/_/  \__/_/  /_/\_,_/\_, /\__/_/   
-                                  /___/          """
+                                  /___/          {0}""".format(url)
